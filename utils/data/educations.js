4c412bb88bc6d7479ba2b5e43fa8c62e66e9c5ca
Mouad Omlil <mouadom2003@gmail.com>
2024-07-08 12:55:40 +0100

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1, 
+    title: "Mern-Stack Qualification Certificate",
+    duration: "2023 - 2024",
+    institution: "JobInTech/Ynov",
+  },
+  {
+    id: 2,
+    title: "Specialised Technicien diploma",
+    duration: "2021 - 2023",
+    institution: "ISGI Casablanca",
+  },
+  {
+    id: 3,
+    title: "Baccaloreat Degree",
+    duration: "2019 - 2020",
+    institution: "Al Mountada Sc",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Website Developer',
+    company: "Hypermag",
+    duration: "(Jul2022 - Sep2022)"
+  },
+  {
+    id: 2,
+    title: "Web Application Developer",
+    company: "CDMA Solution",
+    duration: "(Apr 2023 - Jun 2023)"
+  },
+  {
+    id: 3,
+    title: "Web FullStack Developer",
+    company: "Cyberground",
+    duration: "(Jun 2024 - Present)"
+  }
+]
